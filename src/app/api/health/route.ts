ac2232a01b24a78d3e44907ba334f80390c5257c
Priyobroto Kar <priyobrotokar@gmail.com>
2024-06-30 21:56:18 +0530

    fix: fix pre-commit hook

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -4,5 +4,7 @@ import { NextResponse } from 'next/server'
 export async function GET() {
   const users = await prisma.user.findMany()
 
-  return NextResponse.json(users[0].email, { status: 200 })
+  return NextResponse.json(users.length ? users[0].email : 'No Users', {
+    status: 200
+  })
 }
