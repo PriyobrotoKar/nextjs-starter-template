ee71d3133eb37868a95ace4c1cfc9428df088abf
Priyobroto Kar <priyobrotokar@gmail.com>
2024-06-30 21:02:51 +0530

    feat: add database seed

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   const users = await prisma.user.findMany()
-  console.log(users[0].email)
 
   return NextResponse.json(users[0].email, { status: 200 })
 }
